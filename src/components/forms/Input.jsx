e56239d537afb101978f71625b1ae48354cef030
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-01-15 15:45:39 -0300

    Finalizando

--- src/components/forms/Input.jsx
+++ src/components/forms/Input.jsx
@@ -1,9 +1,10 @@
 import styles from './Input.module.css'
 
-function Input({handleOnChange, type, name, placeholder}){
+function Input({handleOnChange, type, name, placeholder, text}){
     
     return( 
-    <>
+    <> 
+        <p className={styles.texto}>{text}</p>
         <input type={type} placeholder={placeholder}  onChange={handleOnChange} name={name} className={styles.input}/>
     </>
     )
